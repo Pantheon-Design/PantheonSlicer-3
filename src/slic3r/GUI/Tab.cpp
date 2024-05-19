86791306beca96c5527310ebc42f9a5136fa128b
A Koolen-Bourke <akoolenbourke@users.noreply.github.com>
2024-05-20 02:20:08 +1200

    Added a deactivate() function to Tab so when switching tabs it hides … (#5386)
    Added a deactivate() function to Tab so when switching tabs it hides the active contents. Also removed clearing of the pages so it's not destroyed each time.
    
    Co-authored-by: SoftFever <softfeverever@gmail.com>

--- src/slic3r/GUI/Tab.cpp
+++ src/slic3r/GUI/Tab.cpp
@@ -5198,6 +5198,10 @@ bool Tab::tree_sel_change_delayed(wxCommandEvent& event)
     if (m_active_page == page)
         return false;
 
+    if (m_active_page != nullptr) {
+        m_active_page->deactivate();
+    }
+
     m_active_page = page;
 
     auto throw_if_canceled = std::function<void()>([this](){
@@ -5215,8 +5219,6 @@ bool Tab::tree_sel_change_delayed(wxCommandEvent& event)
 
     try {
         m_page_view->Freeze();
-        // clear pages from the controls
-        clear_pages();
         throw_if_canceled();
 
         //BBS: GUI refactor
@@ -5919,6 +5921,13 @@ void Page::activate(ConfigOptionMode mode, std::function<void()> throw_if_cancel
         }
     });
 #endif
+}
+
+void Page::deactivate()
+{
+    for (auto group : m_optgroups) {
+        group->Hide();
+    }
 }
 
 void Page::clear()
