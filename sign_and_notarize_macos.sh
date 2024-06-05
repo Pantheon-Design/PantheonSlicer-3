22bd1cdc7eae5fdf3d1c2cf292630c5a2d7dd376
Alex Wiecke <alex@pantheondesign.ca>
2024-06-05 15:03:20 -0700

    initial working build

---
+++ sign_and_notarize_macos.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+if [[ $(uname -m) == 'arm64' ]]; then
+    Build="build_arm64"
+elif [[ $(uname -m) == 'x86_64' ]]; then
+    Build="build_x86_64"
+fi
+
+BundleName="PantheonSlicer-3"
+Contents="$BundleName.app/Contents"
+Executable="${Contents}/MacOS/$BundleName"
+SIGN_IDENTITY="Developer ID Application: Pantheon Design Ltd. (LXVCBJ7HN9)"
+
+pushd $Build
+if [ -f "$BundleName.dmg" ]; then
+    echo "Deleting existing image at $BundleName.dmg"
+    rm "$BundleName.dmg"
+fi
+
+if [ -f "$BundleName.zip" ]; then
+    echo "Deleting existing $BundleName.zip"
+    rm "$BundleName.zip"
+fi
+
+if [ -f "$BundleName.tar" ]; then
+    echo "Deleting existing $BundleName.tar"
+    rm "$BundleName.tar"
+fi
+
+if [ -f "temp.dmg" ]; then
+    echo "Deleting existing temp.dmg"
+    rm "temp.dmg"
+fi
+
+if [ -d "pack" ]; then
+    echo "Deleting existing pack folder"
+    rm -r "pack"
+fi
+
+
+codesign --force --verbose --options runtime --timestamp -s "$SIGN_IDENTITY" "$BundleName/$BundleName.app/Contents/MacOS/$BundleName"
+
+ln -s /Applications $BundleName/Applications
+hdiutil create -volname "$BundleName" -srcfolder "$BundleName" -ov -format UDZO $BundleName.dmg
+codesign --force --verbose --options runtime --timestamp -s "$SIGN_IDENTITY" $BundleName.dmg
+xcrun notarytool submit "$BundleName.dmg" --wait --keychain-profile "notarytool-password"
+xcrun stapler staple "$BundleName.dmg"
+
+popd
+    
