264a852a0b4ba04ddeb43d6bc3ff01ab1876efdb
Kunlong Ma <kunlong.ma@bambulab.com>
2024-09-04 18:21:00 +0800

    ENH: Optimize the presentation of error code dialog
    JIRA: STUDIO-7469 STUDIO-7477 STUDIO-7613
    
    Change-Id: I6689dc375a51a1d690b82f9dbaa79d1555f7816f
    (cherry picked from commit 777c16fef0862587f3ec468652aabd70cff88dfe)

--- src/slic3r/GUI/BindDialog.cpp
+++ src/slic3r/GUI/BindDialog.cpp
@@ -760,6 +760,7 @@ PingCodeBindDialog::~PingCodeBindDialog() {
          json j = json::parse(str.utf8_string());
          if (j.contains("err_code")) {
              int error_code = j["err_code"].get<int>();
+             wxGetApp().get_hms_query()->query_print_error_msg(error_code, extra);
          }
      }
      catch (...) {
--- src/slic3r/GUI/Jobs/BindJob.cpp
+++ src/slic3r/GUI/Jobs/BindJob.cpp
@@ -105,6 +105,9 @@ void BindJob::process(Ctl &ctl)
             try
             {
                 error_code = stoi(result_info);
+                wxString error_msg;
+                wxGetApp().get_hms_query()->query_print_error_msg(error_code, error_msg);
+                result_info = error_msg.ToStdString();
             }
             catch (...) {
                 ;
