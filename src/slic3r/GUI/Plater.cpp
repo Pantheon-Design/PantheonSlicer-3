342a6f2c003009ee8a36c8b43f26f1b9e136d793
chunmao.guo <chunmao.guo@bambulab.com>
2024-11-04 14:36:18 +0800

    FIX: Force load ams list when click sync button
    Change-Id: I579adc9586e0bca85c20be2784b7513361b38f7e
    Jira: none
    (cherry picked from commit 9c1afdc63c3b1f451d0c8e344eb476e490d1988d)

--- src/slic3r/GUI/Plater.cpp
+++ src/slic3r/GUI/Plater.cpp
@@ -1752,6 +1752,11 @@ void Sidebar::load_ams_list(std::string const &device, MachineObject* obj)
 
 void Sidebar::sync_ams_list()
 {
+    // Force load ams list
+    auto obj = wxGetApp().getDeviceManager()->get_selected_machine();
+    if (obj)
+        GUI::wxGetApp().sidebar().load_ams_list(obj->dev_id, obj);
+
     auto & list = wxGetApp().preset_bundle->filament_ams_list;
     if (list.empty()) {
         MessageDialog dlg(this,
