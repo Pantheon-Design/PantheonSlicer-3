3cbc93f89ec0d9b0aae5cfcf5805f9454b6af12b
azio25134 <azio@pantheondesign.ca>
2024-12-19 11:53:41 -0800

    bumped version number in vendor json

--- src/libslic3r/AppConfig.cpp
+++ src/libslic3r/AppConfig.cpp
@@ -40,6 +40,8 @@ namespace Slic3r {
         static const std::string VERSION_CHECK_URL_STABLE = "https://api.github.com/repos/Pantheon-Design/PantheonSlicer-3/releases/latest";
 static const std::string VERSION_CHECK_URL = "https://api.github.com/repos/Pantheon-Design/PantheonSlicer-3/releases";
 static const std::string PROFILE_UPDATE_URL = "https://api.github.com/repos/Pantheon-Design/PantheonSlicer-3-Profiles/releases";
+// for testing
+// static const std::string         PROFILE_UPDATE_URL = "https://api.github.com/repos/Aziooo/testprofile/releases";
 
 static const std::string MODELS_STR = "models";
 
