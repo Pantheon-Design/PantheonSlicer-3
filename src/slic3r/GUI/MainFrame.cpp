00371da55978b873b13cf9815327ad495046a568
azio25134 <azio@pantheondesign.ca>
2024-12-18 17:42:57 -0800

    renamed Tolerance test back to OrcaToleranceTest

--- src/slic3r/GUI/MainFrame.cpp
+++ src/slic3r/GUI/MainFrame.cpp
@@ -2883,10 +2883,10 @@ void MainFrame::init_menubar_as_editor()
         }, "", nullptr,
         [this]() {return m_plater->is_view3D_shown();; }, this);
         
-    append_menu_item(m_topbar->GetCalibMenu(), wxID_ANY, _L("Pantheon Tolerance Test"), _L("Pantheon Tolerance Test"),
+    append_menu_item(m_topbar->GetCalibMenu(), wxID_ANY, _L("Orca Tolerance Test"), _L("Orca Tolerance Test"),
         [this](wxCommandEvent&) {
             m_plater->new_project();
-        m_plater->add_model(false, Slic3r::resources_dir() + "/calib/tolerance_test/PantheonToleranceTest.stl");
+        m_plater->add_model(false, Slic3r::resources_dir() + "/calib/tolerance_test/OrcaToleranceTest.stl");
         }, "", nullptr,
         [this]() {return m_plater->is_view3D_shown();; }, this);
     // Advance calibrations
