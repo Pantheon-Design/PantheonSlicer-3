489a6554b6388fff58e6d00f6f2e75a3bb277f4a
π² <pi.squared.studio@gmail.com>
2025-07-25 10:36:18 +0300

    Feature: Fuzzy Skin Extrusion Mode (#9878)
    * Feature: Fuzzy Skin Extrusion Mode
    
    This extension allows you to add new features to the fuzzy skin generator.
    
    * Add auto switch to Arachne mode
    
    * Move dialog to `update_print_fff_config` and update how `is_msg_dlg_already_exist` is used
    
    ---------
    
    Co-authored-by: Noisyfox <timemanager.rick@gmail.com>

--- src/slic3r/GUI/Tab.cpp
+++ src/slic3r/GUI/Tab.cpp
@@ -2456,6 +2456,7 @@ page = add_options_page(L("Others"), "custom-gcode_other"); // ORCA: icon only v
 
         optgroup = page->new_optgroup(L("Fuzzy Skin"), L"fuzzy_skin");
         optgroup->append_single_option_line("fuzzy_skin", "others_settings_special_mode#fuzzy-skin");
+        optgroup->append_single_option_line("fuzzy_skin_mode");
         optgroup->append_single_option_line("fuzzy_skin_noise_type", "others_settings_special_mode#fuzzy-skin-mode");
         optgroup->append_single_option_line("fuzzy_skin_point_distance", "others_settings_special_mode#point-distance");
         optgroup->append_single_option_line("fuzzy_skin_thickness", "others_settings_special_mode#skin-thickness");
