c66aab3dcc48bef8238d70200361bd33b1c718b2
azio25134 <azio@pantheondesign.ca>
2025-08-28 15:54:59 -0700

    added a setting to enable low poly support, 30% support gcode reduction

--- src/libslic3r/Support/TreeSupport3D.cpp
+++ src/libslic3r/Support/TreeSupport3D.cpp
@@ -3522,7 +3522,7 @@ static std::pair<float, float> extrude_branch(
     Vec3d nprev;
     Vec3d ncurrent;
     assert(path.size() >= 2);
-    static constexpr const float eps = 0.015f;
+    const float eps = config.low_poly_tree ? 0.8f : 0.015f;
     std::pair<int, int> prev_strip;
 
 //    char fname[2048];
--- src/slic3r/GUI/Tab.cpp
+++ src/slic3r/GUI/Tab.cpp
@@ -2278,6 +2278,7 @@ void TabPrint::build()
         optgroup->append_single_option_line("support_on_build_plate_only");
         optgroup->append_single_option_line("support_critical_regions_only");
         optgroup->append_single_option_line("support_remove_small_overhang");
+        optgroup->append_single_option_line("low_poly_tree");
         //optgroup->append_single_option_line("enforce_support_layers");
 
         optgroup = page->new_optgroup(L("Raft"), L"param_raft");
