f859f4b4a25699be60fe6c0e928be11ed090b045
azio25134 <azio@pantheondesign.ca>
2025-09-23 19:45:32 -0700

    Reapply "renamed the low poly param name, and made it only appear for organic tree setting"
    This reverts commit e812fa0e36e8e3413de4b5891d395f2f2877c0d1.

--- src/libslic3r/PrintConfig.cpp
+++ src/libslic3r/PrintConfig.cpp
@@ -4417,7 +4417,7 @@ void PrintConfigDef::init_fff_params()
     def->mode = comAdvanced;
     def->set_default_value(new ConfigOptionBool(true));
 
-    def           = this->add("low_poly_tree", coBool);
+    def           = this->add("tree_support_low_poly_tree", coBool);
     def->label    = L("Low-poly organic tree support");
     def->category = L("Support");
     def->tooltip  = L("Low-poly organic tree support helps reducing the gcode file size.");
@@ -6264,7 +6264,7 @@ void PrintConfigDef::handle_legacy(t_config_option_key &opt_key, std::string &va
         "retraction_distance_when_cut",
         "extruder_type",
         "internal_bridge_support_thickness","extruder_clearance_max_radius", "top_area_threshold", "reduce_wall_solid_infill","filament_load_time","filament_unload_time",
-        "smooth_coefficient", "overhang_totally_speed", "low_poly_tree"
+        "smooth_coefficient", "overhang_totally_speed"
     };
 
     if (ignore.find(opt_key) != ignore.end()) {
