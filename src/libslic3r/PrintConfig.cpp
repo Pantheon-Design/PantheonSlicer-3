7ef162664cd258192f15b9c95b410a4fcda7e2a9
azio25134 <azio@pantheondesign.ca>
2025-10-17 16:22:16 -0700

    fixed typo

--- src/libslic3r/PrintConfig.cpp
+++ src/libslic3r/PrintConfig.cpp
@@ -3908,11 +3908,11 @@ void PrintConfigDef::init_fff_params()
 
 
     def = this->add("slow_down_before_retraction_speed", coFloat);
-    def->label   = L("Slow down before seam");
+    def->label   = L("Slow down to this speed before seam");
     def->tooltip = L(
         "Slowing down before the seam can help releasing the pressure build-up in the nozzle, results in better seam quality. \n\n"
         "10 is recommended. 0 will disable this feature.");
-    def->sidetext = L("mm/s²");
+    def->sidetext = L("mm/s");
     def->min      = 0;
     def->mode     = comAdvanced;
     def->set_default_value(new ConfigOptionFloat(0));
