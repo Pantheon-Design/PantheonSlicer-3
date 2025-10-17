62e8021b4b23def0efdf41d9dd8ded0127a63870
azio25134 <azio@pantheondesign.ca>
2025-10-16 17:53:27 -0700

    pre retract pressure release. bugged:long straight extrude not working properly

--- src/libslic3r/PrintConfig.hpp
+++ src/libslic3r/PrintConfig.hpp
@@ -1190,6 +1190,8 @@ PRINT_CONFIG_CLASS_DEFINE(
     ((ConfigOptionFloat,               max_volumetric_extrusion_rate_slope_segment_length))
     ((ConfigOptionBool,               extrusion_rate_smoothing_external_perimeter_only))
 
+    ((ConfigOptionFloat,               pressure_release_before_retraction_speed))
+    ((ConfigOptionFloat,               pressure_release_before_retraction_length))
     
     ((ConfigOptionPercents,            retract_before_wipe))
     ((ConfigOptionFloats,              retraction_length))
