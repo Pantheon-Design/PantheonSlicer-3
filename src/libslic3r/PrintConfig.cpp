78b2dc10225e0969fcc5090e3ad46015f2df8c9c
Ian Bassi <ian.bassi@outlook.com>
2025-10-20 00:00:20 -0300

    Limit Shrinkage values (#10930)
    * Limit Shrinkage values
    
    Co-Authored-By: Rodrigo <162915171+RF47@users.noreply.github.com>
    
    * Min 50 Max 150
    
    ---------
    
    Co-authored-by: Rodrigo <162915171+RF47@users.noreply.github.com>

--- src/libslic3r/PrintConfig.cpp
+++ src/libslic3r/PrintConfig.cpp
@@ -2109,7 +2109,8 @@ void PrintConfigDef::init_fff_params()
         "\nBe sure to allow enough space between objects, as this compensation is done after the checks.");
     def->sidetext = "%";
     def->ratio_over = "";
-    def->min = 10;
+    def->min = 50;
+    def->max = 150;
     def->mode = comAdvanced;
     def->set_default_value(new ConfigOptionPercents{ 100 });
     
@@ -2120,7 +2121,8 @@ void PrintConfigDef::init_fff_params()
         " The part will be scaled in Z to compensate.");
     def->sidetext = "%";
     def->ratio_over = "";
-    def->min = 10;
+    def->min = 50;
+    def->max = 150;
     def->mode = comAdvanced;
     def->set_default_value(new ConfigOptionPercents{ 100 });
 
